24258ee9a21b6695c6dc6cd1a044da7ba2cba24c
rizwanalvi <32450360+rizwanalvi@users.noreply.github.com>
2018-07-03 14:44:42 +0500

    Login and Logindb

---
+++ AptechPortalDemo/AptechPortalDemo/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AptechPortalDemo.Models
+{
+    public class Login
+    {
+        public int ID { get; set; }
+        public String UNAME { get; set; }
+        public String UPASS { get; set; }
+        public int ROLEID { get; set; }
+    }
+}
